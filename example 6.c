61718fdac465fe0b762a9a2dcd8eb31c5b28346f
osw0312 <128661045+osw0312@users.noreply.github.com>
2023-04-27 11:48:04 +0900

    Add files via upload

---
+++ example 6.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+int main( void )
+{
+    double pi = 3.1415926535;
+    double x, y;
+    
+x = pi / 2;
+y = sin( x );
+printf( "sin( %f ) = %f\n", x, y );
+y = cos( x );
+printf( "cos( %f ) = %f\n", x, y );
+} 
